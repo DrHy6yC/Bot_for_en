1d90b86b384aaac4dbb0da16146d46ccaf4f8e9f
denisigorevich <dr.hy6yc@gmail.com>
2024-02-04 20:39:43 +0300

    Добавил меню Добавил кнопку отмены в выбор теста При удалении сообщения удаляется  Статус машины состояния Создал процелуру и функцию для просмотра уровня пользователя на основе его последнего прохождения большого теста, если тест не был пройден бот предлагает его пройти добавил клавиатуры

--- Survey/Survey.py
+++ Survey/Survey.py
@@ -1,6 +1,20 @@
+def getLevelUser(balls_percent: float) -> str:
+    if balls_percent == 0:
+        levelUser = "No level"
+    elif 1 <= balls_percent <= 30:
+        levelUser = "Beginner (A1)"
+    elif 30 < balls_percent <= 60:
+        levelUser = "Elementary (A2)"
+    elif 60 < balls_percent <= 90:
+        levelUser = "Pre-Intermediate (B1)"
+    else:
+        levelUser = "Intermediate (B2)"
+    return levelUser
+
+
 class Survey:
     pass
 
 
 if __name__ == '__main__':
-    s = Survey()
+    print(getLevelUser(67))
